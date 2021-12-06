fbc44e0094ce72505ea39ea45cca1424d43c82e6
Jordy Vazquez <jobnimbus@SirJordy.local>
2021-12-06 09:15:38 -0700

    Added the a function that finds none matching brackets and puts it an an array

--- checkCommonBrackets.js
+++ checkCommonBrackets.js
@@ -1,25 +1,30 @@
 let matchingBrackets = function (str) {
-    var openAndClose = {'{':'}'};
-    var stack = [];
-    str = str.replace(/[a-zA-Z0-9\s\?\\\*\&\^\$\#\@\!]/g, '')
+var openAndClose = {'{':'}'};
+var stack = [];
+var text = [];
 
-    for (i=0; i < str.length; i++) {
+for (i=0; i < str.length; i++) {
 
-        if (str[i] === '{') {
-            stack.push(str[i]);
-            console.log(str[i])
-        }else {
-            let last = stack.pop();
-            if(str[i] != openAndClose[last]) {
-                console.log(str[i])
-                return false
-            };
-        }
+    if (str[i] !== '{'|| '}') {
+        text.push(str[i])
     }
-    if (stack.length !==0) {
-        return false;
-    }
-    return true;
+
+    else if (str[i] === '{') {
+        stack.push(str[i]);
+        
+    }else {
+        let last = stack.pop();
+        if(str[i] != openAndClose[last]) {
+            return false
+        };
+    } 
+    
 }
 
-console.log(matchingBrackets('{asdfasdf}'))
+if (stack.length !==0) {
+    return false;
+}
+return true;
+}
+
+console.log(matchingBrackets('{asd{fasdf}'))
